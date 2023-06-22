052c69775c8a21f8696438d2f5bb7a4112d11c2a
DarkShadow390 <angeokou39@gmail.com>
2023-06-22 14:15:27 +0200

    font family

--- chateau.js
+++ chateau.js
@@ -1,79 +1,85 @@
-let imgCrousel1 = document.getElementById("image1");
-let imgCrousel2 = document.getElementById("image2");
-let imgCrousel3 = document.getElementById("image3");
+let imgCarousel1 = document.getElementById("image1");
+let imgCarousel2 = document.getElementById("image2");
+let imgCarousel3 = document.getElementById("image3");
 
-let discCrousel1 = document.getElementById("disc1");
-let discCrousel2 = document.getElementById("disc2");
-let discCrousel3 = document.getElementById("disc3");
+let discCarousel1 = document.getElementById("disc1");
+let discCarousel2 = document.getElementById("disc2");
+let discCarousel3 = document.getElementById("disc3");
 
 
 
-discCrousel1.addEventListener("click", function(){
-    imgCrousel1.style.display = "inline-block";
-    imgCrousel2.style.display = "none";
-    imgCrousel3.style.display = "none";
+discCarousel1.addEventListener("click", function(){
+    imgCarousel1.style.display = "inline-block";
+    imgCarousel2.style.display = "none";
+    imgCarousel3.style.display = "none";
+    discCarousel1.style.backgroundColor = "#260E12";
+    discCarousel2.style.backgroundColor = "bisque";
+    discCarousel3.style.backgroundColor = "bisque";
+    
 
 });
-discCrousel2.addEventListener("click", function(){
-    imgCrousel2.style.display = "inline-block";
-    imgCrousel1.style.display = "none";
-    imgCrousel3.style.display = "none";
+discCarousel2.addEventListener("click", function(){
+    imgCarousel2.style.display = "inline-block";
+    imgCarousel1.style.display = "none";
+    imgCarousel3.style.display = "none";
+    discCarousel2.style.backgroundColor = "#260E12";
+    discCarousel1.style.backgroundColor = "bisque";
+    discCarousel3.style.backgroundColor = "bisque";
 });
-discCrousel3.addEventListener("click", function(){
-    imgCrousel3.style.display = "inline-block";
-    imgCrousel1.style.display = "none";
-    imgCrousel2.style.display = "none";
+discCarousel3.addEventListener("click", function(){
+    imgCarousel3.style.display = "inline-block";
+    imgCarousel1.style.display = "none";
+    imgCarousel2.style.display = "none";
+    discCarousel3.style.backgroundColor = "#260E12";
+    discCarousel1.style.backgroundColor = "bisque";
+    discCarousel2.style.backgroundColor = "bisque";
 });
-
-
-
-
 
 
 let flecheGauche = document.getElementById("flecheGauche");
 let flecheDroite = document.getElementById("flecheDroite");
 
 function Carousel(){
-    if(imgCrousel1){
+    if(imgCarousel1){
         flecheGauche.addEventListener("click", function(){
             imgCrousel3.style.display = "inline-block";
             imgCrousel1.style.display = "none";
             imgCrousel2.style.display = "none";
         });
     };
-    if(imgCrousel1){
+    if(imgCarousel1){
         flecheDroite.addEventListener("click", function(){
-            imgCrousel2.style.display = "inline-block";
-            imgCrousel1.style.display = "none";
-            imgCrousel3.style.display = "none";
+            imgCarousel2.style.display = "inline-block";
+            imgCarousel1.style.display = "none";
+            imgCarousel3.style.display = "none";
         });
     };
-    if(imgCrousel2){
+    if(imgCarousel2){
         flecheGauche.addEventListener("click",function(){
-            imgCrousel1.style.display = "inline-block";
-            imgCrousel2.style.display = "none";
-            imgCrousel3.style.display = "none";
+            imgCarousel1.style.display = "inline-block";
+            imgCarousel2.style.display = "none";
+            imgCarousel3.style.display = "none";
         });
     };
-    if(imgCrousel2){
+    if(imgCarousel2){
         flecheDroite.addEventListener("click", function(){
-            imgCrousel3.style.display = "inline-block";
-            imgCrousel1.style.display = "none";
-            imgCrousel2.style.display = "none";
+            imgCarousel3.style.display = "inline-block";
+            imgCarousel1.style.display = "none";
+            imgCarousel2.style.display = "none";
         });
     };
-    if(imgCrousel3){
+    if(imgCarousel3){
         flecheGauche.addEventListener("click", function(){
-            imgCrousel2.style.display = "inline-block";
-            imgCrousel1.style.display = "none";
-            imgCrousel3.style.display = "none";
+            imgCarousel2.style.display = "inline-block";
+            imgCarousel1.style.display = "none";
+            imgCarousel3.style.display = "none";
         });
     };
-    if(imgCrousel3){
+    if(imgCarousel3){
         flecheDroite.addEventListener("click", function(){
-            imgCrousel1.style.display = "inline-block";
-            imgCrousel2.style.display = "none";
-            imgCrousel3.style.display = "none";
+            imgCarousel1.style.display = "inline-block";
+            imgCarousel2.style.display = "none";
+            imgCarousel3.style.display = "none";
         });
     };
 
